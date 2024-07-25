9c3ec1c7f429b763ea10185e33cc17bc1f90ea33
Arthur <torrezaniarthur@gmail.com>
2024-07-25 13:40:02 -0300

    lista encadeada circular no languagetweet

--- src/project/LanguageTweet.java
+++ src/project/LanguageTweet.java
@@ -1,36 +1,50 @@
 package project;
 
-public class LanguageTweet{
-     private Tweet primeiro;
+public class LanguageTweet {
+     private Tweet head;
      private int amount;
      private int positive;
      private int negative;
      private int neutral;
 
-     public LanguageTweet(){
+     public LanguageTweet() {
+          head = new Tweet("void", "void", "void");
+          head.setProx(head);
+     }
+
+     public void insertInFinal(Tweet t){
+          t.setProx(head);
+          head.setProx(t);
      }
 
      public int getamount() {
           return amount;
      }
+
      public void setamount(int amount) {
           this.amount = amount;
      }
+
      public int getpositives() {
           return positive;
      }
+
      public void setpositives(int positive) {
           this.positive = positive;
      }
+
      public int getNegativos() {
           return negative;
      }
+
      public void setNegativos(int negative) {
           this.negative = negative;
      }
+
      public int getNeutros() {
           return neutral;
      }
+
      public void setNeutros(int neutral) {
           this.neutral = neutral;
      }
